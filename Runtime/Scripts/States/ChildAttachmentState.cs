ad37adb18ca5162e23c8d6dfde7c0e75c3c6ca80
MMMaellon <mmmaellon@gmail.com>
2023-03-07 22:26:41 -0800

    added child attachment state

---
+++ Runtime/Scripts/States/ChildAttachmentState.cs
@@ -0,0 +1,60 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace MMMaellon
+{
+    public class ChildAttachmentState : SmartObjectSyncState
+    {
+        void Start()
+        {
+
+        }
+        public override void OnEnterState(){
+            if (!sync.rigid.isKinematic)
+            {
+                //don't work with physics
+                ExitState();
+                return;
+            }
+            sync.startPos = transform.localPosition;
+            sync.startRot = transform.localRotation;
+            if (sync.IsLocalOwner())
+            {
+                sync.pos = transform.localPosition;
+                sync.rot = transform.localRotation;
+            }
+        }
+
+        public override void OnExitState(){
+        
+        }
+
+        public override void OnSmartObjectSerialize(){
+        
+        }
+
+        public override void OnInterpolationStart(){
+        
+        }
+        
+        public override void Interpolate(float interpolation){
+            transform.localPosition = sync.HermiteInterpolatePosition(sync.startPos, Vector3.zero, sync.pos, Vector3.zero, interpolation);
+            transform.localRotation = sync.HermiteInterpolateRotation(sync.startRot, Vector3.zero, sync.rot, Vector3.zero, interpolation);
+        }
+        
+        public override bool OnInterpolationEnd(){
+            return false;
+        }
+
+        public override void OnDrop()
+        {
+            if (sync.IsLocalOwner() && sync.rigid.isKinematic)
+            {
+                EnterState();
+            }
+        }
+    }
+}
