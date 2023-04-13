31ba16c3fbd214f578520ca049384bdc0a57e6fb
MMMaellon <mmmaellon@gmail.com>
2023-04-12 21:48:58 -0700

    extended autorespawn

--- Runtime/Scripts/Extras/AutoRespawn.cs
+++ Runtime/Scripts/Extras/AutoRespawn.cs
@@ -9,13 +9,16 @@ namespace MMMaellon
     [RequireComponent(typeof(SmartObjectSync))]
     public class AutoRespawn : SmartObjectSyncListener
     {
+        [System.NonSerialized]
         public SmartObjectSync sync;
         public override void OnChangeOwner(SmartObjectSync s, VRCPlayerApi oldOwner, VRCPlayerApi newOwner)
         {
             
         }
-        
-        public float lastSleepTime = -1001f;
+        public bool ignorePhysicsEvents = true;
+        public float respawnCooldown = 30f;
+        [System.NonSerialized]
+        private float lastSleepTime = -1001f;
         public override void OnChangeState(SmartObjectSync s, int oldState, int newState)
         {
             if (s != sync || !sync.IsLocalOwner())
@@ -24,9 +27,20 @@ namespace MMMaellon
             }
             if (newState == SmartObjectSync.STATE_SLEEPING)
             {
-                lastSleepTime = Time.realtimeSinceStartup;
-                SendCustomEventDelayedSeconds(nameof(Respawn), lastSleepTime);
-            } else
+                if (lastSleepTime < 0)
+                {
+                    lastSleepTime = Time.realtimeSinceStartup;
+                    SendCustomEventDelayedSeconds(nameof(Respawn), respawnCooldown);
+                }
+            } else if (ignorePhysicsEvents && (newState == SmartObjectSync.STATE_FALLING || newState == SmartObjectSync.STATE_INTERPOLATING || newState == SmartObjectSync.STATE_TELEPORTING))
+            {
+                if (lastSleepTime < 0)
+                {
+                    lastSleepTime = Time.realtimeSinceStartup;
+                    SendCustomEventDelayedSeconds(nameof(Respawn), respawnCooldown);
+                }
+            }
+            else
             {
                 lastSleepTime = -1001f;
             }
@@ -40,7 +54,7 @@ namespace MMMaellon
 
         public void Respawn()
         {
-            if (lastSleepTime > 0 && lastSleepTime - 0.01f < Time.realtimeSinceStartup)//0.01f for safety against floating point errors
+            if (lastSleepTime > 0 && lastSleepTime + respawnCooldown - 0.01f < Time.realtimeSinceStartup)//0.01f for safety against floating point errors
             {
                 sync.Respawn();
             }
