4c7939e33ad083aa7e471b867f16da13e2ff99bb
MMMaellon <MMMaellon@gmail.com>
2023-06-14 23:47:12 -0700

    added KinematicUntilDropped

---
+++ Packages/com.mmmaellon.smartobjectsync/Runtime/Scripts/Extras/KinematicUntilDropped.cs
@@ -0,0 +1,57 @@
+﻿
+using MMMaellon;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace MMMaellon.SmartObjectSyncExtra
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual), RequireComponent(typeof(SmartObjectSync)), DefaultExecutionOrder(101)]
+    public class KinematicUntilDropped : SmartObjectSyncListener
+    {
+        [System.NonSerialized]
+        public SmartObjectSync sync;
+        bool kinematic = false;
+
+        public bool startKinematic = true;
+        public bool kinematicOnRespawn = true;
+        void Start()
+        {
+            sync = GetComponent<SmartObjectSync>();
+            sync.rigid.isKinematic = startKinematic;
+            kinematic = startKinematic;
+            sync.AddListener(this);
+        }
+
+        public override void OnChangeState(SmartObjectSync s, int oldState, int newState)
+        {
+            if (kinematic)
+            {
+                if (newState != SmartObjectSync.STATE_SLEEPING && !sync.IsHeld() || oldState == SmartObjectSync.STATE_LEFT_HAND_HELD || oldState == SmartObjectSync.STATE_NO_HAND_HELD || oldState == SmartObjectSync.STATE_RIGHT_HAND_HELD)
+                {
+                    sync.rigid.isKinematic = false;
+                    sync.lastKinematic = false;
+                    kinematic = false;
+                }
+                else if (sync.IsHeld())
+                {
+                    sync.lastKinematic = false;
+                }
+            }
+            else
+            {
+                if (newState == SmartObjectSync.STATE_TELEPORTING)
+                {
+                    kinematic = kinematicOnRespawn;
+                    sync.rigid.isKinematic = kinematicOnRespawn;
+                }
+            }
+        }
+
+        public override void OnChangeOwner(SmartObjectSync sync, VRCPlayerApi oldOwner, VRCPlayerApi newOwner)
+        {
+
+        }
+    }
+}
