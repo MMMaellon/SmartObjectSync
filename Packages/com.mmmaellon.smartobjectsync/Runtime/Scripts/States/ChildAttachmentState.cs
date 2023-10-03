4c4651c8d90824ddd11ae984395fed82a5702608
MMMaellon <MMMaellon@gmail.com>
2023-10-02 21:29:01 -0700

    added an option to clear starting parent to child attachments

--- Packages/com.mmmaellon.smartobjectsync/Runtime/Scripts/States/ChildAttachmentState.cs
+++ Packages/com.mmmaellon.smartobjectsync/Runtime/Scripts/States/ChildAttachmentState.cs
@@ -19,6 +19,7 @@ namespace MMMaellon
         [Tooltip("How the child should move when attaching itself to the parent, local to the parent's transform. If this is all zeros then the child just lerps smoothly in place. If this is is (0,0,1), then the child will lerp smoothly to (0,0,1) and then slide along the z axis into place")]
         public Vector3 attachmentVector = Vector3.zero;
         public bool returnToStartingParentOnExit = true;
+        public bool forceNullStartingParent = false;
         public bool automaticallySetTransforms = true;
         [System.NonSerialized, UdonSynced(UdonSyncMode.None), FieldChangeCallback(nameof(parentTransformName))] string _parentTransformName = "";
 
@@ -75,7 +76,14 @@ namespace MMMaellon
         bool firstInterpolation = true;
         void Start()
         {
-            startingParent = transform.parent;
+            if (forceNullStartingParent)
+            {
+                startingParent = null;
+            }
+            else
+            {
+                startingParent = transform.parent;
+            }
             parentTransformName = GetFullPath(startingParent);
         }
         public override void EnterState()
